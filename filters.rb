70338767838964cf6cfd54a63e751a463005ef9c
Upgrayedd0408 <michaellowry89@gmail.com>
2024-05-07 22:27:59 -0700

    add functions to iterate over candidates

--- filters.rb
+++ filters.rb
@@ -2,15 +2,61 @@
 # This way, we keep these methods separated from other potential parts of the program
 
 def find(id)
-    # Your code Here
-  end
+  # Your code Here
+  @candidates.each do |candidate|
+    if candidate['id'] == id
+      return candidate
+    end
+end
   
   def experienced?(candidate)
     # Your code Here
+    if candidate['years_of_experience'] > 2
+      return true
+    else
+      return false
+    end
   end
   
   def qualified_candidates(candidates)
     # Your code Here
+    selected_candidates = []
+    candidates.each do |candidate|
+      if experienced?(candidate) && enough_points(candidate) && tech_stacks(candidate) && age(candidate) && last_applied(candidate)
+        selected_candidates.push(candidate)
+      end
   end
   
   # More methods will go below
+
+  def enough_points(candidate)
+    if candidate['github_points'] >= 100
+      return true
+    else
+      return false
+    end
+  end
+
+  def tech_stacks(candidate)
+    if candidate['languages'].include?('Python' || 'Ruby')
+      return true
+    else
+      return false
+    end
+  end
+
+  def age(candidate)
+    if candidate['age'] > 17
+      return true
+    else
+      return false
+    end
+  end
+
+  def last_applied(candidate)
+    if candidate['date_applied'] < 15.days.ago.to_date
+      return true
+    else
+      return false
+    end
+  end
